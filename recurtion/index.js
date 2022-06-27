9df52ee2f02d752c31dcf641c260212b0866bc99
MrBekzod0899 <qurbonovbekzod1399@gmail.com>
2022-06-28 02:25:19 +0500

    bisektion

--- recurtion/index.js
+++ recurtion/index.js
@@ -6,40 +6,48 @@
 // Oraliqqa berilgan shart  | b - a | <= E(Epsilon)
 
 
-// function func(x,array){
-//     for(let i=0; i<=array.length;i++){
-//         F+=array[i]*Math(x,array.length-i)
+
+// async function getUser() {
+//     try {
+//       const response = await axios.get('http://localhost:3000/page/equation');
+//       console.log(response);
+//     } catch (error) {
+//       console.error(error);
 //     }
-//     return F      
-// }  
+//   }
+
 
 function func(x){
     let F = Math.pow(x,3) + 3*x - 5
     return F
 }
 
+
 let k = 0 
+
 function rekursiya_bisection(a, b, E){              
-    // if ( func(a) * func(b) >= 0){  
-    //     console.log(`Berilgan [${a};${b}] oraliqda tenglamaning ildizi mavjudlik sharti bajarilmadi!!!`)
-    //     console.log(`Ildizni boshqa oraliqda qidirish tavsiya qilinadi`);
-    //     return false
-    //  }      
-    // else if( Math.abs(b-a) >= E){   
-    //         k++
-    //         x = (a+b) / 2
-    //         if(func(a) * func(x) < 0){
-    //             console.log(k + '-qadam: ' + `[${a};${b}] oraliqda x=`+ x + `; f(${x})=${func(x)}`)
-    //             return rekursiya_bisection(a,x,E)
-    //         }
-    //         else {
-    //             console.log(k + '-qadam: ' + `[${a};${b}] oraliqda  x=`+ x + `; f(${x})=${func(x)}`)
-    //             return rekursiya_bisection(x,b,E)
-    //         }                        
-    //     } 
-    //     console.log('Ilzdining berilgan aniqlikdagi taqribiy qiymatini topish uchun sarflangan urinishlar soni:', k)
+    if ( func(a) * func(b) >= 0){  
+        console.log(`Berilgan [${a};${b}] oraliqda tenglamaning ildizi mavjudlik sharti bajarilmadi!!!`)
+        console.log(`Ildizni boshqa oraliqda qidirish tavsiya qilinadi`);
+        return false
+     }      
+    else if( Math.abs(b-a) >= E){   
+            k++
+            x = (a+b) / 2
+            if(func(a) * func(x) < 0){
+                console.log(k + '-qadam: ' + `[${a};${b}] oraliqda x=`+ x + `; f(${x})=${func(x)}`)
+                return rekursiya_bisection(a,x,E)
+            }
+            else {
+                console.log(k + '-qadam: ' + `[${a};${b}] oraliqda  x=`+ x + `; f(${x})=${func(x)}`)
+                return rekursiya_bisection(x,b,E)
+            }                        
+        } 
+        console.log('Ilzdining berilgan aniqlikdagi taqribiy qiymatini topish uchun sarflangan urinishlar soni:', k)
 }
 
 let a,b,E 
-rekursiya_bisection(a, b, E)        
+
+rekursiya_bisection(a, b, E)  
+
 module.exports = {rekursiya_bisection, func}
--- router/page.js
+++ router/page.js
@@ -13,18 +13,33 @@ router.get('/',(req,res)=>{
 
 router.get('/page/equation',async(req,res)=>{  
     let pages = await Page.find().lean()
+    let lastpage= await Page.find().sort({_id:-1}).limit(1).lean()
+    console.log(pages)
+    console.log('last',lastpage)
+    let arr=lastpage[0].arr
+    console.log(arr)
     res.render('page/equation',{
-        pages   
-        
+        pages,
+        lastpage,
+        arr
     })
 })
 
+router.get('/api/equation',async(req,res)=>{
+    let lastpage= await Page.find().sort({_id:-1}).limit(1).lean()
+    let arr=lastpage[0].arr
+    res.send(arr)
+})
+
+router.get('/api/list',async(req,res)=>{
+    let lastpage= await Page.find().sort({_id:-1}).limit(1).lean()
+    res.send(lastpage)
+})
                     
 router.post('/page/equation',async(req,res)=>{
     let {n,a,b,E,arr} = req.body 
     let page = await new Page({n,a,b,E,arr})
     await page.save()
-    console.log(req.body);
     res.redirect('/')
 })
 
